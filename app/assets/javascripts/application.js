0add146c27b5b8344ce3c1f89dbdd40d420f3f4f
taiga-tech <taiga.dev.n@gmail.com>
2020-09-11 01:29:07 +0900

    deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,2 @@
 //= require jquery
-//= require rails-ujs
-//= require popper
-//= require bootstrap-sprockets
-//= require jquery-ui/widgets/sortable
-//= require rails_sortable
 //= require_tree .
