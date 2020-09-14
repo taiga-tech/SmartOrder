2c8e9788cf17cd5d81a997047b73bb91017d8d97
taiga-tech <taiga.dev.n@gmail.com>
2020-09-15 07:14:52 +0900

    update sidebar

--- app/assets/javascripts/mg-modal.js
+++ app/assets/javascripts/mg-modal.js
@@ -1,14 +1,24 @@
 $(function() {
-  $("#mg-bar").click(function() {
-    $(".Side").animate( { width: 'toggle' }, 'fast' )
-    // $(".Side").slideDown("fast")
+
+  function unActive() { $(".Side").addClass('off');
+    $(".Side").animate( { "marginLeft": "0px" }, 400 ).removeClass("on");
+    $(".Main").removeAttr("style");
+  }
+
+  $('#mg-bar').on('click',function(){
+    if ($(".Side").hasClass('off')) {
+      $(".Side").removeClass('off');
+      $(".Side").animate( { "marginLeft": "400px" }, 400 ).addClass("on");
+      $(".Main").css( { "background": "rgba(0, 0, 0, 0.29)" } );
+    } else {
+      unActive();
+    }
   });
-  // $("#mg-bar").hover(
-  //   function() {
-  //     $(".Side").slideDown("fast")
-  //   },
-  //   function() {
-  //     $(".Side").slideUp("fast")
-  //   }
-  // )
+
+  $(".Main").on('click',function(){
+    if ($(".Side").hasClass("on")) {
+      unActive();
+    }
+  })
+
 });
