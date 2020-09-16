523e4c334793fdf3552f57debaf14d5fc58ecff5
taiga-tech <taiga.dev.n@gmail.com>
2020-09-17 01:45:05 +0900

    move side.js

--- app/assets/javascripts/management/side.js
+++ app/assets/javascripts/management/side.js
@@ -2,14 +2,14 @@ $(function() {
 
   function unActive() { $(".Side").addClass('off');
     $(".Side").animate( { "marginLeft": "0px" }, 400 ).removeClass("on");
-    $(".Main").removeAttr("style");
+    // $(".Main").removeAttr("style");
   }
 
   $('#mg-bar').on('click',function(){
     if ($(".Side").hasClass('off')) {
       $(".Side").removeClass('off');
       $(".Side").animate( { "marginLeft": "400px" }, 400 ).addClass("on");
-      $(".Main").css( { "background": "rgba(0, 0, 0, 0.29)" } );
+      // $(".Main").css({"background": "rgba(0, 0, 0, 0.1)", "z-index": "1"});
     } else {
       unActive();
     }
